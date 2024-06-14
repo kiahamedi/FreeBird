0bbb690ae75554d6fbc9b87ab8d2c32f068a81b3
Kia Hamedi <kia.arta9793@gmail.com>
2024-06-14 19:21:54 +0330

    change media name to documents

--- config/settings.py
+++ config/settings.py
@@ -127,8 +127,8 @@ STATICFIELS_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/documents/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'documents')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
