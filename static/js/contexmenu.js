e6837e67e1d866b8bdf87422ccea8184a380db9f
Kia Hamedi <kia.arta9793@gmail.com>
2024-07-04 00:11:23 +0330

    add contex menu

---
+++ static/js/contexmenu.js
@@ -0,0 +1,51 @@
+var $ = jQuery;
+    $(document).ready(function () {
+        'use strict';
+        var errorItems = { "errorItem": { name: "Items Load error" },};
+
+        $.contextMenu({
+            selector: '.context-menu-one',
+            build: function ($trigger, e) {
+                return {
+                    callback: function (key, options) {
+                        var m = "clicked: " + key;
+                        console.log(m);
+                    },
+                    items: {
+                        "uploadfile": { name: "Upload File", icon: "fas fa-file-upload" },
+                        "uploadfolder": { name: "Upload Folder", icon: "fas fa-folder-open" },
+                        "newfolder": { name: "New Folder", icon: "fas fa-folder-plus" },
+                        "sortby": {
+                            name: "Sort By",
+                            icon: "fas fa-sort-amount-down-alt",
+                            items: {
+                                "old": { name: "Old", icon: "fas fa-sort-numeric-down"},
+                                "new": { name: "New", icon: "fas fa-sort-numeric-up"},
+                                "create": { name: "Create", icon: "fas fa-calendar-plus"},
+                            }
+                        }
+                    }
+                };
+            }
+        });
+
+        //normal promise usage example
+        var completedPromise = function (status) {
+            console.log("completed promise:", status);
+        };
+
+        var failPromise = function (status) {
+            console.log("fail promise:", status);
+        };
+
+        var notifyPromise = function (status) {
+            console.log("notify promise:", status);
+        };
+
+        $.loadItemsAsync = function() {
+            console.log("loadItemsAsync");
+            var promise = loadItems();
+            $.when(promise).then(completedPromise, failPromise, notifyPromise);
+        };
+
+    });
