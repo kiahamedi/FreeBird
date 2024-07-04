64c66313ed69f07f9d2f313e7e6cdbbd1eaacbf2
Kia Hamedi <kia.arta9793@gmail.com>
2024-07-05 03:21:09 +0330

    Object Model

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from account.models import User
+from account.models import User, Object
 
 
 UserAdmin.fieldsets[1][1]['fields'] += ("phone",)
 admin.site.register(User, UserAdmin)
+
+admin.site.register(Object)
--- account/models.py
+++ account/models.py
@@ -3,5 +3,36 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
+
+def upload_to(instance, filename):
+    print(instance.owner)
+    return f'drive/{instance.owner.username}/{filename}'
+
+
 class User(AbstractUser):
     phone = models.CharField(null=True, blank=True ,max_length=20 ,verbose_name="Phone")
+
+
+class Object(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner" ,verbose_name="Owner")
+    name = models.CharField(null=False, blank=False, max_length=150 ,verbose_name="Name")
+    iFile = models.BooleanField(default=False, verbose_name="Is File")
+    iFolder = models.BooleanField(default=False, verbose_name="Is Folder")
+    uploadFile = models.FileField(null=True, blank=True, upload_to=upload_to , verbose_name="File")
+    iformat = size = models.CharField(null=True, blank=True, max_length=50 ,verbose_name="Format")
+    size = models.CharField(null=True, blank=True, max_length=50 ,verbose_name="Size")
+    path = models.TextField(null=False, blank=False ,verbose_name="Path")
+    trash = models.BooleanField(default=False, verbose_name="Is Trash")
+    stared = models.BooleanField(default=False, verbose_name="Is Stared")
+    shared = models.BooleanField(default=False, verbose_name="Is Shared")
+    sharedTo = models.ManyToManyField(User, null=True, blank=True, related_name="sharedTo" ,verbose_name="Shared To")
+    sharedLink = models.CharField(null=True, blank=True, max_length=200 ,verbose_name="Shared Link")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        if self.iFile:
+            return self.name + " | File"
+        else:
+            return self.name + " | Folder"
+        
