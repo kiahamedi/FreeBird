0f57554397eb8c44e24c8bdbcbb1f53d79096b7a
Kia Hamedi <kia.arta9793@gmail.com>
2024-07-11 03:40:58 +0330

    write first serializer

---
+++ account/serializers.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import Group
+from account.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
