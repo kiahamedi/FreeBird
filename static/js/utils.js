8e3a7c51d9ad3a7ea6d4ba9978e3556da5e853f2
Kia Hamedi <kia.arta9793@gmail.com>
2024-07-12 03:40:32 +0330

    add function for converter size

--- static/js/utils.js
+++ static/js/utils.js
@@ -2,4 +2,9 @@ function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
-  }
+}
+
+function humanFileSize(size) {
+  var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+  return +((size / Math.pow(1024, i)).toFixed(2)) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+}
