13e708cf79c7d6250f13ff01762ebb6b39a18d95
Kia Hamedi <kia.arta9793@gmail.com>
2024-07-21 04:07:54 +0330

    add setcookie function

--- static/js/utils.js
+++ static/js/utils.js
@@ -34,6 +34,13 @@ function getCookie(name) {
     if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
+function setCookie(c_name, value) {
+  var exdate = new Date();
+  exdate.setDate(exdate.getDate() + 10);
+  var c_value = escape(value) + ((10 == null) ? "" : "; expires=" + exdate.toUTCString());
+  document.cookie = c_name + "=" + c_value;
+}
+
 function humanFileSize(size) {
   var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
   return +((size / Math.pow(1024, i)).toFixed(2)) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
