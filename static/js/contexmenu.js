e5c67234b6b054eb38a73bdd229cc600e3c9f36b
Kia Hamedi <kia.arta9793@gmail.com>
2024-09-21 20:24:59 +0330

    add copy move and paste buttons with stages

--- static/js/contexmenu.js
+++ static/js/contexmenu.js
@@ -3,6 +3,32 @@ var $ = jQuery;
         'use strict';
         var errorItems = { "errorItem": { name: "Items Load error" },};
 
+        var itemscpm = {
+            "sortby": {
+                name: "Sort By",
+                icon: "fas fa-sort-amount-down-alt",
+                items: {
+                    "old": { name: "Old", icon: "fas fa-sort-numeric-down"},
+                    "new": { name: "New", icon: "fas fa-sort-numeric-up"},
+                    "create": { name: "Create", icon: "fas fa-calendar-plus"},
+                }
+            },
+            "newfolder": { name: "New Folder", icon: "fas fa-folder-plus" },
+            "moveobject": { name: "Move", icon: "fas fa-exchange-alt" },
+            "copyobject": { name: "Copy", icon: "fas fa-copy" },
+            "pasteobject": { name: "Paste", icon: "fas fa-paste", visible: function(key, opt) {
+                if (getCookie('is_cm_stage') == '1'){
+                    return true;
+                } else {
+                    return false;
+                }
+                }},
+            "uploadfile": { name: "Upload File", icon: "fas fa-file-upload" },
+            "uploadfolder": { name: "Upload Folder", icon: "fas fa-folder-open" },
+        }
+    
+
+        
         $.contextMenu({
             selector: '.context-menu-one',
             build: function ($trigger, e) {
@@ -14,22 +40,18 @@ var $ = jQuery;
                             $('#modal-upload-file').modal('toggle');
                         }else if (key == "newfolder"){
                             $('#modal-create-folder').modal('toggle');
+                        }else if (key == "moveobject"){
+                            setCookie("is_cm_stage", '1');
+
+                        }else if (key == "copyobject"){
+                            setCookie("is_cm_stage", '1');
+
+                        }else if (key == "pasteobject"){
+                            setCookie("is_cm_stage", '0');
+
                         };
                     },
-                    items: {
-                        "uploadfile": { name: "Upload File", icon: "fas fa-file-upload" },
-                        "uploadfolder": { name: "Upload Folder", icon: "fas fa-folder-open" },
-                        "newfolder": { name: "New Folder", icon: "fas fa-folder-plus" },
-                        "sortby": {
-                            name: "Sort By",
-                            icon: "fas fa-sort-amount-down-alt",
-                            items: {
-                                "old": { name: "Old", icon: "fas fa-sort-numeric-down"},
-                                "new": { name: "New", icon: "fas fa-sort-numeric-up"},
-                                "create": { name: "Create", icon: "fas fa-calendar-plus"},
-                            }
-                        }
-                    }
+                    items: itemscpm
                 };
             }
         });
@@ -53,4 +75,5 @@ var $ = jQuery;
             $.when(promise).then(completedPromise, failPromise, notifyPromise);
         };
 
-    });
+});
+
